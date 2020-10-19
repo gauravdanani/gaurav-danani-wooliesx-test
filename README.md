22df0dc58c77ddb343d24db36e926b21aae5a39c
Gaurav Danani <gaurav.danani@gmail.com>
2020-10-20 10:09:53 +1100

    Updated README and tried Optional Exercise 3

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gaurav-danani-wooliesx-test
+##How to test:
+#### Token: 6055169b-11e0-4504-99b5-984624465858
+
+####Exercise 1:
+
+url: "https://gaurav-danani-wooliesx-test.azurewebsites.net/api/users"
+
+####Exercise 2:
+
+url: "https://gaurav-danani-wooliesx-test.azurewebsites.net/api/products"
+
+####Exercise 3:
+
+url: "https://gaurav-danani-wooliesx-test.azurewebsites.net/api/trolleys" 
+
+## Notes 
+* I have added unit tests for only 1 class to demonstrate testing.
+* Added Few ToDos for further improvements.
+* I tried Optional part of Exercise 3 for an hour but couldn't finish it.
+* We should add Component and Contract tests as well.
--- src/Gaurav.Danani.WooliesX.Application/Trolleys/Queries/GetTrolleyTotal/GetTrolleyTotalQueryHandler.cs
+++ src/Gaurav.Danani.WooliesX.Application/Trolleys/Queries/GetTrolleyTotal/GetTrolleyTotalQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Gaurav.Danani.WooliesX.Application.Common.Interfaces;
@@ -18,6 +19,52 @@ namespace Gaurav.Danani.WooliesX.Application.Trolleys.Queries.GetTrolleyTotal
         public async Task<decimal> Handle(GetTrolleyTotalQuery request, CancellationToken cancellationToken)
         {
             return  await _trolleyService.GetTrolleyTotal(request.Trolley);
+        }
+
+        private decimal GetTrolleyTotalInternal(GetTrolleyTotalQuery request, CancellationToken cancellationToken)
+        {
+            decimal trolleyTotal = 0;
+            var trolleyProductNames = request.Trolley.Quantities.Select(q => q.Name);
+            var special = request.Trolley.Specials.FirstOrDefault(s =>
+                s.Quantities.Select(q => q.Name).SequenceEqual(trolleyProductNames));
+
+            int minQuantity = request.Trolley.Quantities.Min(q => q.Quantity);
+
+            if (special != null)
+            {
+                foreach (var trolleyQuantity in request.Trolley.Quantities)
+                {
+                    var productPrice = request.Trolley.Products.First(p => p.Name == trolleyQuantity.Name).Price;
+                    for (var i = 1; i <= trolleyQuantity.Quantity; i++)
+                    {
+                        if (i <= minQuantity)
+                        {
+                            var specialQty = special.Quantities.FirstOrDefault(sq => sq.Name == trolleyQuantity.Name);
+                            var multiplier = trolleyQuantity.Quantity / specialQty.Quantity;
+
+                            if (multiplier < 1)
+                            {
+                                trolleyTotal += i * productPrice;
+                            }
+                            else
+                            {
+                                //var remainder = specialQty.Quantity % trolleyQuantity.Quantity;
+                                trolleyTotal += (special.Total / special.Quantities.Length);
+                            }
+                        }
+                        else
+                        {
+                            trolleyTotal += i * productPrice;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            return trolleyTotal;
         }
     }
 }
